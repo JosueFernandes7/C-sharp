10943735528fc35642f56aad24b5682f728e23be
Josue Fernandes <josue7mf@hotmail.com>
2023-04-24 15:21:03 -0300

    metodos staticos

---
+++ Projetos/Produto/Produto/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Course
+{
+    class Calculadora
+    {
+        public static double PI = 3.14; 
+        public static double Circunferencia(double r)
+        {
+            return 2 * PI * r;
+        }
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * PI * r * r * r;
+        }
+    }
+}
---
+++ Projetos/Produto/Produto/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is Produto)
+            {
+                Produto newObj = (Produto)obj;
+                return newObj.Preco == Preco && newObj.Quantidade == Quantidade && newObj.Nome == Nome;
+            }
+            return false;
+        }
+    }
+}
