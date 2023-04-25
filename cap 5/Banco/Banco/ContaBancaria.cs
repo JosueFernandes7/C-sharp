fe5aaf8c2b1d61a8968428aa826564d36ddc9cf6
Josue Fernandes <josue7mf@hotmail.com>
2023-04-25 15:47:48 -0300

    contaBancaria.cs

---
+++ cap 5/Banco/Banco/ContaBancaria.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+namespace Banco
+{
+    class ContaBancaria
+    {
+        private int _numero;
+        private string _titular;
+        private double _saldo;
+
+        public int Numero { get => _numero; set => _numero = value; }
+        public double Saldo { get => _saldo; set => _saldo = value; }
+        public string Titular { get => _titular; set => _titular = value; }
+
+        public ContaBancaria(int numero,string titular)
+        {
+            _numero = numero;
+            _titular = titular;
+        }
+
+
+        public void deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void saque(double quantia)
+        {
+            if (quantia >= Saldo)
+            {
+                Saldo -= quantia;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta " +
+                    Numero + ", " +
+                    "Titular: " + Titular +
+                    ", Saldo: $ " + Saldo;
+   
+        }
+    }
+
+}
---
+++ cap 5/Banco/Banco/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Globalization;
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titularConta = Console.ReadLine();
+            ContaBancaria conta = new ContaBancaria(numeroConta, titularConta);
+            bool depositoInicial = Console.ReadLine().Contains('s');
+            if(depositoInicial)
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                conta.deposito(double.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+        }
+
+    }
+
+}
