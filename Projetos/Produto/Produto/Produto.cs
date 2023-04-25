d85f84e123bf9f759c3aa7ea576d046322e38b82
Josue Fernandes <josue7mf@hotmail.com>
2023-04-25 15:22:35 -0300

    getters e setters

--- Projetos/Produto/Produto/Produto.cs
+++ Projetos/Produto/Produto/Produto.cs
@@ -1,36 +1,43 @@
 ﻿using System.Globalization;
-
 namespace Course
 {
     class Produto
     {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+
+        private string nome;
+        private double Preco;
+        private int Quantidade;
+
+        public string Nome { get => nome; set => nome = value; }
+
+        public Produto(string nome, double preco, int quantidade) 
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
         public double ValorTotalEmEstoque()
         {
             return Preco * Quantidade;
         }
-
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
         public override string ToString()
         {
             return Nome
-                + ", $ "
-                + Preco.ToString("F2", CultureInfo.InvariantCulture)
-                + ", "
-                + Quantidade
-                + " unidades, Total: $ "
-                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if(obj is Produto)
-            {
-                Produto newObj = (Produto)obj;
-                return newObj.Preco == Preco && newObj.Quantidade == Quantidade && newObj.Nome == Nome;
-            }
-            return false;
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
