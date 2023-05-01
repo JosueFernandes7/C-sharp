61bd69262004d8c8951718055ff0b5ac5bd2e802
Josue Fernandes <josue7mf@hotmail.com>
2023-05-01 12:07:16 -0300

    ex matriz

---
+++ Projetos/Nullable/Nullable/Calculator.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Nullable
+{
+    class Calculator
+    {
+        public static void Triple(ref int x)
+        {
+            x = x * 3;
+        }
+        public static void Triple(int x, out int result)
+        {
+            result = x * 3;
+        }
+        public static int Sum (params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int item in numbers)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Projetos/Nullable/Nullable/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            List<int> list = new List<int>() { 1, 2, 3};
+            list.Add(1);
+            list.Add(4);
+            list.Add(2);
+            Console.WriteLine(list);
+            list.ForEach(x => Console.WriteLine(x));
+        }
+
+    }
+}
